6ec0320048672b4dfbb389d0e53f0dadde94a111
TheLoneLion <110144987+TheLoneLion@users.noreply.github.com>
2022-09-15 13:21:45 +1000

    Add files via upload

--- climate.py
+++ climate.py
@@ -8,15 +8,14 @@ temp = climate['Temperature'].to_numpy()
 
 
 plt.subplot(2, 1, 1)
-plot(years, co2, 'b--') 
+plt.plot(years, co2, 'b--')
 plt.title("Climate Data") 
 plt.ylabel("[CO2]") 
 plt.xlabel("Year (decade)") 
 
 plt.subplot(2, 1, 2)
-plot(years, temp, 'r*-') 
+plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp.png") 
-
